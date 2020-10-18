eb59af133e7f4ac90cede9ca30790d4a8a3b7be5
bodo79 <rubinstein.elad@gmail.com>
2020-10-18 19:49:51 +0300

    add URL example

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,6 +8,9 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit}) => {
 			<p className='f3'>
 				{'put a URL of a photo to detect the faces in it'}
 			</p>
+			<p className='f3'>
+				{`copy & paste => https://i2-prod.mirror.co.uk/incoming/article14334083.ece/ALTERNATES/s615/3_Beautiful-girl-with-a-gentle-smile.jpg`}
+			</p>
 			<div className='center'>
 				<div className='form center pa4 br3 shadow-5'>
 					<input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
